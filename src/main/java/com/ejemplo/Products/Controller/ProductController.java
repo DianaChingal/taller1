a151f805b31bc7c0445ff7dbeb4ce135f80f6ddc
dianachingal <dianachingalb@gmail.com>
2025-05-29 12:22:11 -0500

    milton

--- src/main/java/com/ejemplo/Products/Controller/ProductController.java
+++ src/main/java/com/ejemplo/Products/Controller/ProductController.java
@@ -1,21 +1,22 @@
 package com.ejemplo.Products.Controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ejemplo.Products.Dto.ProductDto;
 import com.ejemplo.Products.Entity.Product;
 import com.ejemplo.Products.Service.ProductService;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 
@@ -40,6 +41,28 @@ public class ProductController {
         return new ResponseEntity<>(listProduct, HttpStatus.OK);
     }
     
-    
+   @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id,  @RequestBody ProductDto productDto) {
+        Product updatedProduct = productService.updateProduct(id, productDto);
+        return new ResponseEntity<>(updatedProduct, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT); // 204: Eliminado con éxito
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        Product product = productService.getProductById(id);
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+
+    @GetMapping("/valor")
+    public ResponseEntity<List<ProductDto>> getProductsByValor(@RequestParam double valor) {
+        List<ProductDto> productos = productService.getProductsByValor(valor);
+        return new ResponseEntity<>(productos, HttpStatus.OK);
+    }
 
 }
